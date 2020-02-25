5712f53b69117744df994acfe0f361580928bb6f
Joao Filipe Rocha <jfsllrocha@gmail.com>
2020-02-25 23:01:08 +0000

    Updates

---
+++ precache-manifest.402141c18e30a9d6691a96196c7b4007.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf663833942f125c807ce50aa212d90",
+    "url": "/bin2dec/index.html"
+  },
+  {
+    "revision": "144bc88ac6fc0f1948dc",
+    "url": "/bin2dec/static/css/main.eb5a051b.chunk.css"
+  },
+  {
+    "revision": "df7ab1335d233d950b5c",
+    "url": "/bin2dec/static/js/2.e1aa9274.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bin2dec/static/js/2.e1aa9274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "144bc88ac6fc0f1948dc",
+    "url": "/bin2dec/static/js/main.7c38e4f1.chunk.js"
+  },
+  {
+    "revision": "b783a336d5b59451da62",
+    "url": "/bin2dec/static/js/runtime-main.5d6ee3d1.js"
+  }
+]);
