e8985515e6aceb17bd5978ff441903d112c08a50
Joao Filipe Rocha <jfsllrocha@gmail.com>
2020-02-25 22:23:40 +0000

    bin2dec Deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [binVal, setBinVal] = useState('');
+    const [decVal, setDecVal] = useState('')
+
+    function binChange(val) {
+
+        if (val.length > 0) {
+            if (val.match(/^[01]+$/g) === null)
+                return;
+
+            const reversedBinaryText = val
+                .split('')
+                .map(Number) // Convert to a number from string
+                .reverse();
+
+            // Calculate the result by accumulating previous vaue
+            const result = reversedBinaryText.reduce(
+                (accumulator, currentValue, idx) =>
+                    accumulator + currentValue * Math.pow(2, idx)
+            );
+            setDecVal(result);
+        }
+        else {
+            setDecVal('');
+        }
+
+        setBinVal(val);
+    }
+
+    return (
+        <div className='container'>
+            <div className="divTable">
+                <div className="divTB">
+                    <div className="divTR">
+                        <div className="divTD">
+                            Binario:
+                    </div>
+                        <div className="divTD">
+                            <input
+                                placeholder="Digite o numero Binario"
+                                maxLength="8"
+                                value={binVal}
+                                onChange={e => binChange(e.target.value)} /></div>
+                    </div>
+                    <div className="divTR">
+                        <div className="divTD">
+                            Decimal:
+                    </div>
+
+                        <div className="divTD">
+                            <input
+                                placeholder="Numero Decimal"
+                                className="decVal"
+                                value={decVal}
+                                readOnly /></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
 }
 
 export default App;
